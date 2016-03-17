5e29d91796a6f1ac7740f5ff38c8262f1046cd99
Leah Bentley <leahbent@outlook.com>
2016-03-17 17:41:47 -0600

    fix tests, add new test

--- src/tooltip.spec.js
+++ src/tooltip.spec.js
@@ -13,23 +13,30 @@ describe('tooltip', function() {
 			'</div>'
 		);
 
+		dismissClickElm = angular.element(
+			'<div>' +
+				'<input type="text" cp-tooltip="hello" cp-tooltip-dismiss-on-click>' +
+			'</div>'
+		);
+
 		$('body').append(elm);
+		$('body').append(dismissClickElm);
 
 		scope = $rootScope.$new();
 
 		$compile(elm)(scope);
+		$compile(dismissClickElm)(scope);
 	}));
 
 	afterEach(function() {
 		jasmine.clock().uninstall();
 		scope.$broadcast('$destroy');
 		elm.remove();
+		dismissClickElm.remove();
 	});
 
 	it('Should display the tooltip when the mouse moves over', function() {
-		var e = $.Event('mousemove');
-		e.clientX = 100;
-		e.clientY = 200;
+		var e = $.Event('mouseover');
 
 		elm.find('input').trigger(e);
 		jasmine.clock().tick(1001);
@@ -38,43 +45,23 @@ describe('tooltip', function() {
 		expect(tooltip.length).toBe(1);
 	});
 
-	it('Should correctly position the tooltip', function() {
-		var e = $.Event('mousemove');
-		e.clientX = 100;
-		e.clientY = 200;
-
-		elm.find('input').trigger(e);
-		jasmine.clock().tick(1001);
-		var tooltip = $('.cp-tooltip');
-		var position = tooltip.position();
-		expect(position.left).toBe(100);
-		expect(position.top).toBe(220);
-	});
-
 	it('Should correctly throttle displaying the tooltip', function() {
-		var e = $.Event('mousemove');
-		e.clientX = 100;
-		e.clientY = 200;
+		var e = $.Event('mouseover');
 
 		elm.find('input').trigger(e);
 		jasmine.clock().tick(500);
 
-		e = $.Event('mousemove');
-		e.clientX = 100;
-		e.clientY = 210;
+		expect($('.cp-tooltip').length).toBe(0);
+
+		e = $.Event('mouseover');
 		elm.find('input').trigger(e);
 		jasmine.clock().tick(1001);
 
-		var tooltip = $('.cp-tooltip');
-		var position = tooltip.position();
-		expect(position.left).toBe(100);
-		expect(position.top).toBe(230);
+		expect($('.cp-tooltip').length).toBe(1);
 	});
 
 	it('Should hide the tooltip on mouseout', function() {
-		var e = $.Event('mousemove');
-		e.clientX = 100;
-		e.clientY = 200;
+		var e = $.Event('mouseover');
 
 		elm.find('input').trigger(e);
 		jasmine.clock().tick(1001);
@@ -87,4 +74,33 @@ describe('tooltip', function() {
 
 		expect($('.cp-tooltip').length).toBe(0);
 	});
+
+	it('Should not hide the tooltip until clicked outside when using the cp-tooltip-dismiss-on-click attr', function() {
+		var e = $.Event('mouseover');
+
+		dismissClickElm.find('input').trigger(e);
+		jasmine.clock().tick(1001);
+
+		expect($('.cp-tooltip').length).toBe(1);
+
+		e = $.Event('mouseout');
+		dismissClickElm.find('input').trigger(e);
+		jasmine.clock().tick(501);
+
+		expect($('.cp-tooltip').length).toBe(1);
+
+		e = $.Event('click');
+		$('.cp-tooltip').trigger(e);
+
+		jasmine.clock().tick(501);
+
+		expect($('.cp-tooltip').length).toBe(1);
+
+		e = $.Event('click');
+		dismissClickElm.find('input').trigger(e);
+		jasmine.clock().tick(501);
+
+		expect($('.cp-tooltip').length).toBe(0);
+	});
+
 });
